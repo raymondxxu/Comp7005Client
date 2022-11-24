0f04c589f8210adea8c633604fc77eb5cd017ac5
Raymond <raymondxxu>
2022-11-23 23:03:43 -0800

    Update Code

--- Sources/Client/Client.swift
+++ Sources/Client/Client.swift
@@ -4,10 +4,18 @@ import CommonLib
 @main
 public struct Client {
     
+    private static func readUserInput() {
+        
+    }
+
     public static func main() {
         let argParser = ArgParser.senderArgParser
         var initalDataId = 0
-        
+        var sendedData = [DataModel]()
+        var sendingData = [DataModel]()
+        var receivedData = [Int]()
+        var sendingCounts = 0
+        var receingCount = 0
         do {
             try argParser.parse()
         } catch(let error) {
@@ -31,20 +39,46 @@ public struct Client {
             exit(-1)
         }
         print("connection establihsed")
-        while true {
-            let readingLine = readLine()
-            if readingLine?.count == 0 {
-                break
-            }
-            let objectData = DataModel(seq: initalDataId, type: .SYN, data: readingLine)
-            let cStr = (objectData as JsonStringConvertible).convert()! as NSString
-            write(socketManager.socketFD!, cStr.cString(using: String.Encoding.ascii.rawValue), cStr.length)
+        
+        let readingLine = readLine() ?? ""
+        let objectData = DataModel(seq: initalDataId, type: .SYN, data: readingLine)
+        sendedData.append(objectData)
+        var lastReceivedId = -1
+        var addMissingData = true
+        while let current = sendedData.popLast() {
+            sendingData.append(current)
+            let cStr = (current as JsonStringConvertible).convert()! as NSString
+            print(cStr)
+            sendingCounts += 1
+            let sendingBytes = write(socketManager.socketFD!, cStr.cString(using: String.Encoding.ascii.rawValue), cStr.length)
+            print("sending \(sendingBytes)")
             initalDataId += 1
+            Thread.sleep(forTimeInterval: 0.5)
             var receivedBuffer = Array<CChar>(repeating: 0, count: 1024)
+            var timeout = timeval(tv_sec: 1, tv_usec: 0)
+            setsockopt(socketManager.socketFD!, SOL_SOCKET, SO_RCVTIMEO, &timeout, socklen_t(MemoryLayout.size(ofValue: timeout)))
             let bytes = read(socketManager.socketFD!, &receivedBuffer, 1024)
-            let json = (DataModel.convert(from: String(utf8String: receivedBuffer)!) as? DataModel)!
-            print(String(cString: receivedBuffer))
-            print(json)
+            if bytes > 0 {
+                receingCount += 1
+            }
+            if let json = (DataModel.convert(from: String(utf8String: receivedBuffer)!) as? DataModel) {
+                receivedData.append(json.id)
+            }
+            if let readingLine = readLine() {
+                let objectData = DataModel(seq: initalDataId, type: .SYN, data: readingLine)
+                sendedData.append(objectData)
+            } else {
+                for sendingDatum in sendingData {
+                    if !receivedData.contains(sendingDatum.id) {
+                         sendedData.append(sendingDatum)
+                    } else {
+                        sendedData.removeAll{ $0.id == sendingDatum.id }
+                    }
+                }
+            }
+            print("number of packets sended: \(sendingCounts)")
+            print("number of packets received: \(receingCount)")
+            print("Actual Drop \(abs(100 - Double(receingCount) / Double(sendingCounts) * 100))%")
         }
         
     }
